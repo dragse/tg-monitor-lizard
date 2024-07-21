8f006780eb88d58e6d4d11ab6e5e8d23f435d0ce
Lennard Brinkhaus <lennard.brinkhaus@dragse.de>
2024-07-21 14:22:03 +0200

    wip: continue with error

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,14 @@
+use teloxide::RequestError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum LizardError {
     #[error("unknown error: {0}")]
     Unknown(String)
+}
+
+impl Into<RequestError> for LizardError {
+    fn into(self) -> RequestError {
+        RequestError::
+    }
 }
