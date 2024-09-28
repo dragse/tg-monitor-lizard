7aae9de95ee9a76a9b14c501b724a395eb2fcf29
Lennard Brinkhaus <lennard.brinkhaus@dragse.de>
2024-09-28 21:53:29 +0200

    feat: implement basic example with frankenstein

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,13 @@ use std::env;
 
 use dotenvy::dotenv;
 use env_logger::TimestampPrecision;
+use frankenstein::{Api, GetUpdatesParams, ReplyParameters, SendMessageParams, TelegramApi, UpdateContent};
 use log::info;
 use serde_json::json;
-use teloxide::prelude::*;
 
-use crate::model::{GroupConfiguration, JoinValidation};
-
-mod etcd;
-mod model;
-mod telegram;
 pub mod util;
 pub mod error;
+pub mod db;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -24,8 +20,41 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let telegram_token =
         env::var("TELEGRAM_TOKEN").expect("'TELEGRAM_TOKEN' is an required environment variable");
-    let telegram_bot = Bot::new(telegram_token);
+    let api = Api::new(telegram_token.as_str());
 
+    let bot_info = api.get_me()?;
+    info!("Connect to Telegram with Bot: @{:?}", bot_info.result.username);
+
+    let mut update_params = GetUpdatesParams::builder().build();
+    loop {
+        let result = api.get_updates(&update_params);
+
+        println!("result: {result:?}");
+
+        match result {
+            Ok(response) => {
+                for update in response.result {
+                    if let UpdateContent::Message(message) = update.content {
+                        let reply_parameters = ReplyParameters::builder()
+                            .message_id(message.message_id)
+                            .build();
+                        let send_message_params = SendMessageParams::builder()
+                            .chat_id(message.chat.id)
+                            .text("hello")
+                            .reply_parameters(reply_parameters)
+                            .build();
+                        if let Err(error) = api.send_message(&send_message_params) {
+                            println!("Failed to send message: {error:?}");
+                        }
+                    }
+                    update_params.offset = Some(i64::from(update.update_id) + 1);
+                }
+            }
+            Err(error) => {
+                println!("Failed to get updates: {error:?}");
+            }
+        }
+    }
 
     Ok(())
 }
